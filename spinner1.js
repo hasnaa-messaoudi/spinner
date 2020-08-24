47f5eb8f32a48de153799995d374512ac4a80efa
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-08-24 21:09:17 +0000

    add spinner not refactored :D

--- spinner1.js
+++ spinner1.js
@@ -1,13 +1,38 @@
-const chars = ['|', '/', '-', '\\', '|', '/', '-', '\\'];
-let t = 100;
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
 
-for (let char of chars) {
-  t += 200;
-  setTimeout(() => {
-    process.stdout.write(`\r${char}   `);
-  }, t);
-}
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1500);
 
 setTimeout(() => {
   process.stdout.write('\n');
-}, t + 200);
+}, 1700);
+
